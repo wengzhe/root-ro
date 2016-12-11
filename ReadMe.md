ce88e8d2beb55ca7f9ecbb4952a93ceea90ea3ed
翁喆 <zhe_weng@163.com>
2016-12-11 17:47:11 +0800

    初版基本完成

---
+++ ReadMe.md
@@ -0,0 +1,9 @@
+## V1.0
+
+- 使用overlayfs在initramfs实现的Read-Only文件系统
+- 使用niun/root-ro @ gist.github.com
+- 使用install.sh安装，uninstall.sh恢复
+- 使用switch.sh切换状态（需要重启）
+
+
+翁喆 2016.12.11
--- uninstall.sh
+++ uninstall.sh
@@ -9,7 +9,7 @@ fi
 if [ -e "/etc/initramfs-tools/scripts/init-bottom/root-ro" ]; then
     echo "root-ro found, removing"
     rm /etc/initramfs-tools/scripts/init-bottom/root-ro
-    update-initramfs -u
+    mkinitramfs -o /boot/init.gz
 fi
 
 if [ -e "/boot/cmdline.bak" ]; then
@@ -17,4 +17,7 @@ if [ -e "/boot/cmdline.bak" ]; then
     mv /boot/cmdline.bak /boot/cmdline.txt
 fi
 
-echo "done! please reboot"
+echo "Done! Please reboot"
+echo "The initramfs still exists, you can manually remove it:"
+echo "1. Delete the /boot/init.gz file"
+echo "2. Remove the line \"initramfs init.gz\" in /boot/config.txt"
