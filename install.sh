890818ca2003a009fda0a14ccda5c63a1f4c06d6
翁喆 <zhe_weng@163.com>
2016-12-11 17:01:46 +0800

    做了一点修改

--- install.sh
+++ install.sh
@@ -25,10 +25,23 @@ if [ -e "/boot/cmdline.bak" ] && [ -e "/etc/initramfs-tools/scripts/init-bottom/
     fi
 fi
 
-echo "installing script \"root-ro\""
+#checking initramfs
+num=`find /boot -name 'init*' | wc -l`
+echo "Checking initramfs"
+if [ $num -lt 1 ]; then
+	echo "Initramfs missing"
+	mkinitramfs -o /boot/init.gz
+	if [ `cat /boot/config.txt | grep 'initramfs' | wc -l` -lt 1 ]; then
+		echo "initramfs init.gz" >> /boot/config.txt
+	fi
+	echo "Reboot to make sure initramfs work well please"
+	exit 0
+fi
+echo "Initramfs good"
+
+echo "Installing script \"root-ro\""
 
 cp root-ro /etc/initramfs-tools/scripts/init-bottom/root-ro
-#/usr/share/initramfs-tools/scripts/init-bottom
 chmod 0755 /etc/initramfs-tools/scripts/init-bottom/root-ro
 
 #add overlay to /etc/initramfs-tools/modules
@@ -37,31 +50,19 @@ if [ `cat /etc/initramfs-tools/modules | grep 'overlay' | wc -l` -lt 1 ]; then
 fi
 
 #now we need to make sure there is an initramfs exist
-num=`find /boot -name 'init*' | wc -l`
-echo "updating initramfs"
-if [ $num -lt 1 ]; then
-	update-initramfs -c -k `uname -r` -v
-	if [ `cat /boot/config.txt | grep 'initramfs' | wc -l` -lt 1 ]; then
-		echo "initramfs initrd.img-`uname -r`" >> /boot/config.txt
-	fi
-	#mkinitramfs -o /boot/initrd.img-`uname -r`
-	#initramfs initrd.img-`uname -r` @ config.txt
-	#initramfs initrd.img-4.4.22-v7+ 0x00f00000
-else
-	update-initramfs -u
-fi
+echo "Updating initramfs"
+mkinitramfs -o /boot/init.gz
 
-echo "script is installed, changing /boot/cmdline.txt"
+echo "Ccript is installed, changing /boot/cmdline.txt"
 
-echo "backup /boot/cmdline.txt to /boot/cmdline.bak"
+echo "Backing up /boot/cmdline.txt to /boot/cmdline.bak"
 cp /boot/cmdline.txt /boot/cmdline.bak
 
-echo "adding root-ro-driver=overlay disable-root-ro=false to /boot/cmdline.txt"
+echo "Adding root-ro-driver=overlay disable-root-ro=false to /boot/cmdline.txt"
 bak=`cat /boot/cmdline.txt`
 echo -e "root-ro-driver=overlay disable-root-ro=false \c" > /boot/cmdline.txt
-#initrd=0xf00000,0x57BD38				#root=/dev/mmcblk0p2 rootfstype=ext4
 echo $bak >> /boot/cmdline.txt
 
 #or we can use cmdline(string) in config.txt of Raspberry Pi instead of cmdline.txt
 
-echo "done! reboot please"
+echo "Done! Reboot please"
